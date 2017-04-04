2b7aea562bafa2a4e59673ea8f31ae226c0b21d0
dosborn2 <dosborn2@wisc.edu>
2017-04-04 10:20:02 -0500

    Module 9 assignment completed
    sorry its a few min late, it kept saing commit fail

--- js/practice2.js
+++ js/practice2.js
@@ -1,6 +1,4 @@
-//__AUTHOR__DylanOsborn__
-//making these variables global
-var attrArray = ["Percent below poverty line", "percent completed highschool", "Percent black", "Percent asian", "percent hispanic/latino", "percent white"];
+var attrArray = ["varA", "varB", "varC", "varD", "varE"];
 var expressed = attrArray[0]; //initial attribute
 (function(){
 //begin script when window loads
@@ -8,14 +6,14 @@ window.onload = setMap();
 
 //Example 1.3 line 4...set up choropleth map
 function setMap(){
-
-
-    //map frame dimensions
+  //map frame dimensions
     var width = window.innerWidth * 0.5,
-        height = 550;
+        height = 460;
+
     //map frame dimensions
-    // var width = 960,
-    //     height = 550;
+    var width = 960,
+        height = 550;
+
     //create new svg container for the map
     var map = d3.select("body")
         .append("svg")
@@ -23,182 +21,175 @@ function setMap(){
         .attr("width", width)
         .attr("height", height);
 
-    //create Albers equal area conic projection centered on California
+    //create Albers equal area conic projection centered on France
     var projection = d3.geoAlbers()
-        // specifies the [longitude, latitude] coordinates of the center of the plane
-        .center([0, 37.5])
-        //specifies the [longitude, latitude, and roll] angles by which to rotate the globe
-        .rotate([120, 0])
-        //specifies the two standard parallels of a conic projection
-        .parallels([29.5, 45.5])
-        .scale(2900)
-        //offsets the pixel coordinates of the projection's center in the svg container
-        //keep these at one - half the svg width and height to keep map centered in container
+        .center([0, 46.2])
+        .rotate([-2, 0])
+        .parallels([43, 62])
+        .scale(2500)
         .translate([width / 2, height / 2]);
 
-    //holds the path generator
     var path = d3.geoPath()
         .projection(projection);
 
     //use d3.queue to parallelize asynchronous data loading
     d3.queue()
-        .defer(d3.csv, "data/Cali_Data.csv") //load attributes from csv
-        .defer(d3.json, "data/US_States.topojson") //load background spatial data
-        .defer(d3.json, "data/Cali_Counties.topojson") //load choropleth spatial data
+        .defer(d3.csv, "data/unitsData.csv") //load attributes from csv
+        .defer(d3.json, "data/EuropeCountries.topojson") //load background spatial data
+        .defer(d3.json, "data/FranceRegions.topojson") //load choropleth spatial data
         .await(callback);
 
-    //callback function
-    function callback(error, csvData, us, cali){
+    function callback(error, csvData, europe, france){
+
       //place graticule on the map
       setGraticule(map, path);
 
-        //translate europe TopoJSON
-        var unitedStates = topojson.feature(us, us.objects.US_States), //object from US_States.topojson
-            caliCounties = topojson.feature(cali, cali.objects.Cali_Counties).features; //object from Cali_Counties.topojson
+      //translate europe TopoJSON
+      var europeCountries = topojson.feature(europe, europe.objects.EuropeCountries),
+          franceRegions = topojson.feature(france, france.objects.FranceRegions).features;
 
-        //var attrArray = ["Percent below poverty line", "percent completed highschool", "Percent black", "Percent asian", "percent hispanic/latino", "percent white"];
-        //var expressed = attrArray[0]; //initial attribute
-        //add usa states to map
-        var states = map.append("path")
-          .datum(unitedStates)
-          .attr("class", function(d){
-            return "state "; //creates style for states
-          })
-          .attr("d", path);
+      var attrArray = ["varA", "varB", "varC", "varD", "varE"];
+      var expressed = attrArray[0]; //initial attribute
+          //add Europe countries to map
+      var countries = map.append("path")
+        .datum(europeCountries)
+        .attr("class", "countries")
+        .attr("d", path);
 
-
-        var colorScale = makeColorScale(csvData);
+//add France regions to map
+      var colorScale = makeColorScale(csvData);
         //add enumeration units to the map
-        setEnumerationUnits(caliCounties, map, path, colorScale, csvData);
+      setEnumerationUnits(franceRegions, map, path, colorScale, csvData);
 
-        //add coordinated visualization to the map
-        setChart(csvData, colorScale);
-      };
+      //add coordinated visualization to the map
+      setChart(csvData, colorScale);
+};
 }; //end of setMap()
+
 
 //examine the results
 function setGraticule(map, path){
     //...GRATICULE BLOCKS FROM MODULE 8
     //create graticule generator
     var graticule = d3.geoGraticule()
-      .step([10, 10]); //place graticule lines every 5 degrees of longitude and latitude
-    //create graticule background
+      .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
+      //create graticule background
     var gratBackground = map.append("path")
       .datum(graticule.outline()) //bind graticule background
       .attr("class", "gratBackground") //assign class for styling
       .attr("d", path) //project graticule
 
-    //Example 2.6 creates graticule lines
+      //Example 2.6 line 5...create graticule lines
 
-    //create graticule lines
+      //create graticule lines
     var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
       .data(graticule.lines()) //bind graticule lines to each element to be created
       .enter() //create an element for each datum
       .append("path") //append each element to the svg as a path element
       .attr("class", "gratLines") //assign class for styling
       .attr("d", path); //project graticule lines
-
 };
 
-function joinData(caliCounties, csvData){
+function joinData(franceRegions, csvData){
     //...DATA JOIN LOOPS FROM EXAMPLE 1.1
     //variables for data join
-    //loop through csv to assign each set of csv attribute values to geojson county
+
+    //begin script when window loads
+
+    //loop through csv to assign each set of csv attribute values to geojson region
     for (var i=0; i<csvData.length; i++){
-        var csvCounty = csvData[i]; //the current county
-        var csvKey = csvCounty.NAME; //the CSV primary key
+        var csvRegion = csvData[i]; //the current region
+        var csvKey = csvRegion.adm1_code; //the CSV primary key
 
-        //loop through geojson counties to find correct county
-        for (var a=0; a<caliCounties.length; a++){
+        //loop through geojson regions to find correct region
+        for (var a=0; a<franceRegions.length; a++){
 
-            var geojsonProps = caliCounties[a].properties; //the current county geojson properties
-            var geojsonKey = geojsonProps.NAME; //the geojson primary key
-            //console.log(csvKey, geojsonKey);
+            var geojsonProps = franceRegions[a].properties; //the current region geojson properties
+            var geojsonKey = geojsonProps.adm1_code; //the geojson primary key
+            console.log(csvKey, geojsonKey);
             //where primary keys match, transfer csv data to geojson properties object
             if (geojsonKey == csvKey){
 
                 //assign all attributes and values
                 attrArray.forEach(function(attr){
-                    var val = parseFloat(csvCounty[attr]); //get csv attribute value
+                    var val = parseFloat(csvRegion[attr]); //get csv attribute value
                     geojsonProps[attr] = val; //assign attribute and value to geojson properties
                 });
             };
         };
     };
-    return caliCounties;
+    return franceRegions;
 };
 
-function setEnumerationUnits(caliCounties, map, path, colorScale, csvData){
-    caliCounties = joinData(caliCounties, csvData)
+function setEnumerationUnits(franceRegions, map, path, colorScale, csvData){
+    franceRegions = joinData(franceRegions, csvData)
 
-    //...counties BLOCK FROM MODULE 8
-    //var attrArray = ["Percent below poverty line", "percent completed highschool", "Percent black", "Percent asian", "percent hispanic/latino", "percent white"];
-    //var expressed = attrArray[0]; //initial attribute
-    //add France counties to map
-    var counties = map.selectAll(".counties")
-        .data(caliCounties)
+    //...REGIONS BLOCK FROM MODULE 8
+    var attrArray = ["varA", "varB", "varC", "varD", "varE"];
+    var expressed = attrArray[0]; //initial attribute
+    //add France regions to map
+    var regions = map.selectAll(".regions")
+        .data(franceRegions)
         .enter()
         .append("path")
         .attr("class", function(d){
-          return "county " + d.properties.NAME;//creates style for counties
+            return "regions " + d.properties.adm1_code;
         })
         .attr("d", path)
         .style("fill", function(d){
-          //console.log(d.properties, expressed);
-          return colorScale(d.properties[expressed]);
+          console.log(d.properties, expressed);
+            return colorScale(d.properties[expressed]);
         })
         .style("fill", function(d){
-          return choropleth(d.properties, colorScale);
+            return choropleth(d.properties, colorScale);
 
-        });
+});
 };
-//bar graph made the most sense for my data, however if i have more time i would like to try another visualization
 //function to create coordinated bar chart
 function setChart(csvData, colorScale){
     //chart frame dimensions
-    var chartWidth = window.innerWidth * 0.45,
+    var chartWidth = window.innerWidth * 0.425,
         chartHeight = 473,
         leftPadding = 25,
         rightPadding = 2,
         topBottomPadding = 5,
-        //i removed these to fit all my data in the graph
-        // chartInnerWidth = chartWidth - leftPadding - rightPadding,
-        // chartInnerHeight = chartHeight - topBottomPadding * 2,
+        chartInnerWidth = chartWidth - leftPadding - rightPadding,
+        chartInnerHeight = chartHeight - topBottomPadding * 2,
         translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
 
     //create a second svg element to hold the bar chart
     var chart = d3.select("body")
         .append("svg")
-        .attr("width", chartWidth + 11)
+        .attr("width", chartWidth)
         .attr("height", chartHeight)
         .attr("class", "chart");
 
     //create a rectangle for chart background fill
     var chartBackground = chart.append("rect")
         .attr("class", "chartBackground")
-        .attr("width", chartWidth)
-        .attr("height", chartHeight)
+        .attr("width", chartInnerWidth)
+        .attr("height", chartInnerHeight)
         .attr("transform", translate);
 
     //create a scale to size bars proportionally to frame and for axis
     var yScale = d3.scaleLinear()
         .range([463, 0])
-        .domain([0, 35]);
+        .domain([0, 100]);
 
     //set bars for each province
-    var bars = chart.selectAll(".bars")
+    var bars = chart.selectAll(".bar")
         .data(csvData)
         .enter()
-        .append("rect") //adds bars to graph
+        .append("rect")
         .sort(function(a, b){
-            return b[expressed]-a[expressed] // direction of slope
+            return b[expressed]-a[expressed]
         })
         .attr("class", function(d){
-            return "bars " + d.NAME;
+            return "bar " + d.adm1_code;
         })
-        .attr("width", chartWidth / csvData.length - 1) //white space between the bars
+        .attr("width", chartInnerWidth / csvData.length - 1)
         .attr("x", function(d, i){
-            return i * (chartWidth / csvData.length) + leftPadding;
+            return i * (chartInnerWidth / csvData.length) + leftPadding;
         })
         .attr("height", function(d, i){
             return 463 - yScale(parseFloat(d[expressed]));
@@ -207,7 +198,7 @@ function setChart(csvData, colorScale){
             return yScale(parseFloat(d[expressed])) + topBottomPadding;
         })
         .style("fill", function(d){
-            return choropleth(d, colorScale); //adds the color to graph
+            return choropleth(d, colorScale);
         });
 
     //create a text element for the chart title
@@ -215,8 +206,7 @@ function setChart(csvData, colorScale){
         .attr("x", 40)
         .attr("y", 40)
         .attr("class", "chartTitle")
-        // brute force for title of attribute (fix me)
-        .text("Percent of Population " + expressed[8]+ expressed[9]+ expressed[10]+ expressed[11]+ expressed[12]+ expressed[13]+ expressed[14]+ expressed[15]+ expressed[16]+ expressed[17]+ expressed[18]+ expressed[19]+ expressed[20]+ expressed[21] );
+        .text("Number of Variable " + expressed[3] + " in each region");
 
     //create vertical axis generator
     var yAxis = d3.axisLeft()
@@ -238,15 +228,13 @@ function setChart(csvData, colorScale){
 
 //function to create color scale generator
 function makeColorScale(data){
-  //console.log(data);
-    
+  console.log(data);
     var colorClasses = [
-        "#fee5d9",
-        "#fcbba1",
-        "#fc9272",
-        "#fb6a4a",
-        "#de2d26",
-        "#a50f15"
+        "#D4B9DA",
+        "#C994C7",
+        "#DF65B0",
+        "#DD1C77",
+        "#980043"
     ];
 
     //create color scale generator
@@ -264,7 +252,7 @@ function makeColorScale(data){
     };
 
     //cluster data using ckmeans clustering algorithm to create natural breaks
-    var clusters = ss.ckmeans(domainArray, 6);
+    var clusters = ss.ckmeans(domainArray, 5);
     //reset domain array to cluster minimums
     domainArray = clusters.map(function(d){
         return d3.min(d);
@@ -290,3 +278,10 @@ function choropleth(props, colorScale){
 };
 
 })(); //last line of main.js
+var counties = map.selectAll(".counties")
+  .data(caliCounties)
+  .enter()
+  .append("path")
+  .attr("class", function(d){
+    return "county " + d.properties.NAME;//creates style for counties
+  })
